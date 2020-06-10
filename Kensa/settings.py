374a49961de3f8a70765bf14ff185f67a253a354
pxyg13 <carsaroca.13@gmail.com>
2020-06-09 19:40:19 -0500

    rest_framework

--- Kensa/settings.py
+++ Kensa/settings.py
@@ -391,8 +391,8 @@ JAVA_BINARY = find_java_binary()
 
 # ============DJANGO REST========================
 REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 100
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 30
 }
 # ===============================================
 
