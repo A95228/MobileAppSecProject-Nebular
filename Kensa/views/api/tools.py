6b994a4bf2f5d555f73d9d671d3a21cec4be2448
pxyg13 <carsaroca.13@gmail.com>
2020-06-09 19:41:27 -0500

    search tool

--- Kensa/views/api/tools.py
+++ Kensa/views/api/tools.py
@@ -16,24 +16,31 @@ def get_smali_drop(md5):
     if subprocess.run(["which", "find"], capture_output=True).returncode != 0:
         logger.error("Can't find 'find' command, contact sysadmin")
         return None
+    
     try:
         tgt = os.path.join(settings.UPLD_DIR, md5, "smali_source")
     except AttributeError as error:
         logger.error(str(error))
         return None
+    
     logger.info("Running subprocess for smali file search of scan %s" % md5)
     runner = subprocess.run(
             ["find", tgt, "-regex", ".*\.smali$"],
             capture_output=True)
+    
     if runner.returncode != 0:
         msg = "find command returned %s for .smali files search of %s" % (
             runner.returncode, md5)
         logger.error(msg)
+        return None
+
     logger.info("find command search OK for scan %s" % md5)
     drop = runner.stdout.decode('utf-8').split("\n")
+
     if drop.__len__() == 0:
-        logger.info("No .smali files for  scan %s" % md5)
+        logger.info("No .smali files for scan %s" % md5)
         return None
+    
     files = []
     for each in drop:
         match = re.match(r'.+\/smali_source\/(?P<path>.+\.smali)$', each)
@@ -43,15 +50,30 @@ def get_smali_drop(md5):
         if not this.endswith(".smali"):
             continue
         files.append(escape(this))
+    
     context = {
         'title': 'Smali Source',
         'files': sorted(files),
-        '_type': 'apk',
-        '_hash': md5,
+        'type': 'apk',
+        'hash': md5,
         'version': settings.KENSA_VER,
     }
     logger.info("Fetching %s smali files for scan %s" % (len(drop), md5))
     return context 
-    
 
+
+def merge_search(*args):
+    """Helper for search route."""
+    i = []
+    for _ in args[0]:
+        try:
+            i.append(_["MD5"])
+        except KeyError:
+            continue
+    for _ in args[1]:
+        try:
+            i.append(_['MD5'])
+        except KeyError:
+            continue
+    return i
 
