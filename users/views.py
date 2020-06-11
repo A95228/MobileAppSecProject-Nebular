897156a2ef6276652c1f1f1c1de44eb6039c1c14
pxyg13 <carsaroca.13@gmail.com>
2020-06-11 03:16:46 -0500

    User API update

--- users/views.py
+++ users/views.py
@@ -1,25 +1,66 @@
-# Module contains views for Kensa's User API
+"""
+This module contains routes for the User API REST functionality
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+TO DO:
 
+* Must add middleware to check if user is authenticated and
+  if it has permissions to do the operations within the body
+  of the view.
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+"""
+
+import logging
+import pdb
+
+from django.db import IntegrityError
 from django.conf.urls import url
+from django.contrib import messages
+from django.contrib.auth import (
+    authenticate,
+    login,
+    logout,
+    PermissionDenied,
+    update_session_auth_hash,
+)
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 from django.views.generic import UpdateView
-
-
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_exempt
 from rest_framework import serializers, status
 from rest_framework.response import Response
-from django.db import IntegrityError
-from users.models import User
+
 
 from Kensa.views.api.rest_api import make_api_response
 from Kensa.views.helpers import request_method
+from users.models import User
+
+
+logger = logging.getLogger(__name__)
+
+
+BACKENDS = {
+    "allauth" : "allauth.account.auth_backends.AuthenticationBackend",
+    "django" : "django.contrib.auth.backends.ModelBackend"
+}
 
 
 class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('id', 'email', 'name', 'last_name', 'phone_number', 'addr_city', 'addr_state', 'addr_country',
-                  'organization')
+        fields = (
+            'id',
+            'email',
+            'name',
+            'last_name',
+            'phone_number',
+            'addr_city',
+            'addr_state',
+            'addr_country',
+            'organization'
+        )
 
 
 class ProfileView(UpdateView):
@@ -47,139 +88,109 @@ class ProfileView(UpdateView):
 
         return Response(data=UserSerializer(user).data, status=status.HTTP_200_OK)
 
-# Turning these to CBV's
+
+@request_method(["POST"])
+def api_log_in(request): # not tested !
+    """Authenticate a user"""
+    if request.POST.get("email") is None:
+        return make_api_response(
+            {"error" : "missing email"}, 400)
+    if request.POST.get("password") is None:
+        return make_api_response(
+            {"error" : "missing password"}, 400)
+    try:
+        user = authenticate(request.POST["email"],
+                request.POST["password"])
+    except PermissionDenied:
+        return make_api_response(
+            {"error" : "invalid credentials"}, 400)
+    if user is None:
+        return make_api_response({"error" : "invalid credentials"}, 403)
+    if not user.is_active:
+        return make_api_response(
+            {"error" : "Please contact support to reopen your account"}, 403)
+
+    login(request, user, backend=BACKENDS["django"])
+
+    #Flash welcome message to user in frontend.
+    messages.success(request, "Welcome back %s" % request.user.username)
+    return HttpResponseRedirect(reverse("home"))
+    
+
+@request_method(["GET"])
+def api_logout(request):
+    if request.user.is_authenticated:
+        username = request.user.username 
+        logout(request)
+        messages.success(request, message="See you later %s !" % usernme)
+        return HttpResponseRedirect(reverse("home"))
+    else:
+        messages.info(request, message="You can't logout if you are not signed in.")
+        return HttpResponseRedirect(reverse("home"))
+
+
+@request_method(["POST"])
+@login_required
+@csrf_exempt # for testing, remove for production
+def api_edit_password(request):
+    """Edit user password"""
+    if not request.GET.get("password1") or not request.GET.get("password2"):
+        return make_api_response({"error" : "Missing password data"}, 400)
+
+    if request.GET["password1"] != request.GET["password2"]:
+        return make_api_response({"error" : "Password must match"}, 400)
+
+    ok, drop = User.update_password(
+        request.GET["password1"], request.user.pk)
+
+    if not ok:
+        return make_api_response({"error" : drop})
+
+    logout(request) # pop the user out of the session
+
+    return make_api_response({"success" : drop}, 200)
+
 
 @request_method(["POST"])
 def api_edit_email(request):
-    #if not request.user.is_authenticated:
-    #    return make_api_response({"error" : "login and try again"},
-    #        status=401)
-    #if len(request.user.get_user_permissions()) < 0:
-    #    return make_api_response({"error" : "login and try again"},
-    #        status=403)
-    #if request.POST.get("new_email", None) is None:
-    #    error = "New email is required"
-    #    return make_api_response({"error" : error}, status=BAD_REQUEST)
-    #if request.POST.get("new_email") == request.user.email:
-    #    error = "%s can't be the same as current email" % request.POST["email"]
-    #    return make_api_response({"error" : error}, 400)
-    #
-    return make_api_response({}, 404)
-
-
-@request_method(["POST"])
-def api_edit_password(request):
-    #if not request.user.is_authenticated:
-    #    return make_api_response({"error" : "login and try again"},
-    #        status=401)
-    #if len(request.user.get_user_permissions()) < 0:
-    #    return make_api_response({"error" : "login and try again"},
-    #        status=403)
-    # check the password does not match the current password
-    # check the password does not violate the password requirements e.g -
-    # includes !@#$%^&*()_+, longer than 8 etc. . 
-    # if there are two password fields, check they match
-    # change
     return make_api_response({}, 404)
 
 
 @request_method(["POST"])
 def api_edit_username(request):
-    #if not request.user.is_authenticated:
-    #    return make_api_response({"error" : "login and try again"},
-    #        status=401)
-    #if len(request.user.get_user_permissions()) < 0:
-    #    return make_api_response({"error" : "login and try again"},
-    #        status=403)
-    # try to get the username, if the username already exists, show error
-    # check username follows protocol,
-    # change.
+
     return make_api_response({}, 404)
 
 
 @request_method(["POST"])
 def api_edit_image(request):
-    #if not request.user.is_authenticated:
-    #    return make_api_response({"error" : "login and try again"},
-    #        status=401)
-    #if len(request.user.get_user_permissions()) < 0:
-    #    return make_api_response({"error" : "login and try again"},
-    #        status=403)
-    # check the image is not malicious
-    # save the image in the uploads? will this be a cloud service?
     return make_api_response({}, 404)
 
 
 @request_method(["POST"])
 def api_edit_name(request):
-    #if not request.user.is_authenticated:
-    #    return make_api_response({"error" : "login and try again"},
-    #        status=401)
-    #if len(request.user.get_user_permissions()) < 0:
-    #    return make_api_response({"error" : "login and try again"},
-    #        status=403)
     return make_api_response({}, 404)
 
 
 @request_method(["POST"])
 def api_edit_first_name(request):
-    #if not request.user.is_authenticated:
-    #    return make_api_response({"error" : "login and try again"},
-    #        status=401)
-    #if len(request.user.get_user_permissions()) < 0:
-    #    return make_api_response({"error" : "login and try again"},
-    #        status=403)
     return make_api_response({}, 404)
 
 
 @request_method(["POST"])
 def api_edit_last_name(request):
-    #if not request.user.is_authenticated:
-    #    return make_api_response({"error" : "login and try again"},
-    #        status=401)
-    #if len(request.user.get_user_permissions()) < 0:
-    #    return make_api_response({"error" : "login and try again"},
-    #        status=403)
     return make_api_response({}, 404)
 
 
 @request_method(["POST"])
 def api_edit_short_name(request):
-    #if not request.user.is_authenticated:
-    #    return make_api_response({"error" : "login and try again"},
-    #        status=401)
-    #if len(request.user.get_user_permissions()) < 0:
-    #    return make_api_response({"error" : "login and try again"},
-    #        status=403)
     return make_api_response({}, 404)
 
-
-@request_method(["GET"])
-def api_get_user_info(request):
-    #if not request.user.is_authenticated:
-    #    return make_api_response({"error" : "login and try again"},
-    #        status=401)
-    #if len(request.user.get_user_permissions()) < 0:
-    #    return make_api_response({"error" : "login and try again"},
-    #        status=403)
-    # more checks here
-    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    # u = request.user
-    # info = dict(
-    #   email=u.email,
-    #   username=u.username,
-    #   name=u.name,  
-    #   first_name=u.first_name,
-    #   last_name=u.last_name,
-    #   short_name=u.short_name,
-    #   organization=u.organization,
-    #   is_active=u.is_active
-    # )
-    #return make_api_response({"user_info" : info}, status=OK)
-    return make_api_response({}, 404)
 
 
 api_user_urls = [
+    url(r"^api/v1/api_login", api_log_in),
+    url(r"^api/v1/api_logout", api_logout),
     url(r"^api/v1/edit_email$", api_edit_email),
     url(r"^api/v1/edit_password$", api_edit_password),
     url(r"^api/v1/edit_username$", api_edit_username),
@@ -188,5 +199,4 @@ api_user_urls = [
     url(r"^api/v1/edit_first_name$", api_edit_first_name),
     url(r"^api/v1/edit_last_name$", api_edit_last_name),
     url(r"^api/v1/edit_short_name$", api_edit_short_name),
-    url(r"^api/v1/get_user_info$", api_get_user_info),
 ]
