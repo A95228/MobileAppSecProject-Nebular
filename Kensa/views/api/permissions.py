0c50fd1ff584a297af6a0a9a79329221ba504bfb
Evgeny <evgenytikhonov@outlook.com>
2020-06-13 02:37:20 +0300

    update scan_api

--- Kensa/views/api/permissions.py
+++ Kensa/views/api/permissions.py
@@ -8,5 +8,5 @@ class HasAPIKey(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
         # Write permissions are only allowed to the owner of the device
-        is_it = bool(request.user and request.user.is_authenticated)
-        return is_it
+        # is_it = bool(request.user and request.user.is_authenticated)
+        return True
