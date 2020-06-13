e71f56add131a286602c712f2bd4a27b372f7ca5
pxyg13 <37247816+pxyg13@users.noreply.github.com>
2020-06-13 06:51:53 -0500

    TokenError and InvalidToken imports

--- Kensa/views/api/views.py
+++ Kensa/views/api/views.py
@@ -11,6 +11,7 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework_simplejwt.views import (TokenObtainPairView)
+from rest_framework_simplejwt.exceptions import TokenError, InvalidToken
 from Kensa.utils import api_key
 from Kensa.views.api import tools
 from Kensa.views.home import Upload, RecentScans, delete_scan
